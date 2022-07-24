a74fa783b345725dfc06ed17f3ea41cd82c4aa21
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-07-24 20:16:13 +0200

    fix fetching logic retrieving duplicate and missing stuff

--- repos.go
+++ repos.go
@@ -16,14 +16,18 @@ import (
 // 		getAllRepos(ctx, client)
 // }
 
+func getOptions(page int) *github.RepositoryListOptions {
+	return &github.RepositoryListOptions{
+		ListOptions: github.ListOptions{PerPage: 10, Page: page},
+	}
+}
+
 func getAllRepos(ctx context.Context, client *github.Client) []*github.Repository {
 	repos := []*github.Repository{}
 
 	wg := sync.WaitGroup{}
 
-	opt := &github.RepositoryListOptions{
-		ListOptions: github.ListOptions{PerPage: 50, Page: 1},
-	}
+	opt := getOptions(1)
 
 	result, response, err := client.Repositories.List(ctx, "", opt)
 	if err != nil {
@@ -39,12 +43,12 @@ func getAllRepos(ctx context.Context, client *github.Client) []*github.Repositor
 
 	// first page is always retrieved above, so we start iteration
 	// from 2 - which is the page number after 1 :) (c) Cap.Obvious
-	for p := response.NextPage; p < response.LastPage + 1; p++ {
+	for p := response.NextPage; p <= response.LastPage; p++ {
 		wg.Add(1)
 
 		go func(opt *github.RepositoryListOptions, page int) {
-			opt.ListOptions.Page = page
-			reposPerPage, _, err := client.Repositories.List(ctx, "", opt)
+			options := getOptions(page)
+			reposPerPage, _, err := client.Repositories.List(ctx, "", options)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -59,6 +63,6 @@ func getAllRepos(ctx context.Context, client *github.Client) []*github.Repositor
 	for _, i := range repos {
 		log.Println(*i.FullName)
 	}
-	log.Println("LENGTH ", len(repos))
+	log.Println("Amount of repos fetched ", len(repos))
 	return repos
 }
