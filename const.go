49ab0c32bb9bf8ee6843b283a496776b8d9045f4
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-07-29 00:22:41 +0200

    feature consts

--- const.go
+++ const.go
@@ -2,3 +2,6 @@ package main
 
 const SESSION_ERROR_KEY = "session_error"
 const HELP_URL = "https://github.com/OlegGulevskyy"
+
+const REPOSITORIES = "repositories"
+const PULL_REQUESTS = "pull_requests"
